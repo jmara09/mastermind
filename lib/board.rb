ae734f9288f2b273cea5e5b9bc018164c5d136d6
Jmara <jmara.0923@gmail.com>
2024-10-03 15:45:14 +0400

    Add method to change color of board
    The color of the board will change depending on the user's guess

--- lib/board.rb
+++ lib/board.rb
@@ -1,56 +1,72 @@
 # frozen_string_literal: true
 
-require 'colorize'
+module Mastermind
+  class Board
+    COLOR = %w[red blue white black green yellow magenta].freeze
+    TINY_PEG = %w[white black].freeze
 
-class Board
-  def initialize(code)
-    @secret_code = code
-    @board_guess_accuracy = Array.new(12, Array.new(4, '0'))
-    @board_guess_pegs = Array.new(12, Array.new(4, '0'))
-    @guess = []
-  end
+    def initialize
+      @secret_code = []
+      @board_guess_accuracy = Array.new(12, Array.new(4, '0'))
+      @board_guess_pegs = Array.new(12, Array.new(4, '0'))
+      @guess = []
+      @tries = 0
+    end
 
-  protected
+    def guess_pegs(code)
+      @tries += 1
+      @guess << code
+    end
 
-  def guess(code)
-    @guess << code
-  end
+    def game_board
+      top_board(@board_guess_accuracy)
+      bottom_board(@board_guess_pegs)
+    end
 
-  def game_board
-    top_board(@board_guess_accuracy)
-    bottom_board(@board_guess_pegs)
-  end
-
-  private
-
-  def top_board(board)
-    puts "#{'+-----' * 11}+-----+"
-    4.times do |i|
-      12.times do |j|
-        next unless (i + 1).odd?
-
-        if j.zero?
-          print "| #{board[j][i]} #{board[j][i + 1]} | "
-        else
-          print "#{board[j][i]} #{board[j][i + 1]} | "
+    def colorize_bottom
+      @board_guess_pegs = @board_guess_pegs.map.with_index do |column, i|
+        column.map.with_index do |pegs, j|
+          if i <= @tries - 1
+            pegs.colorize(@guess[i][j].to_sym)
+          else
+            pegs
+          end
         end
-        puts "\n" if j == 11
+        # break if idx == @tries
       end
     end
-  end
 
-  def bottom_board(board)
-    puts "#{'+-----' * 11}+-----+"
-    4.times do |i|
-      12.times do |j|
-        if j == 11
-          print "|  #{board[j][i]}  |"
-        else
-          print "|  #{board[j][i]}  "
+    private
+
+    def top_board(board)
+      puts "#{'+-----' * 11}+-----+"
+      4.times do |i|
+        12.times do |j|
+          next unless (i + 1).odd?
+
+          if j.zero?
+            print "| #{board[j][i]} #{board[j][i + 1]} | "
+          else
+            print "#{board[j][i]} #{board[j][i + 1]} | "
+          end
+          puts "\n" if j == 11
         end
       end
-      puts "\n"
     end
-    puts "#{'+-----' * 11}+-----+"
+
+    def bottom_board(board)
+      puts "#{'+-----' * 11}+-----+"
+      4.times do |i|
+        12.times do |j|
+          if j == 11
+            print "|  #{board[j][i]}  |"
+          else
+            print "|  #{board[j][i]}  "
+          end
+        end
+        puts "\n"
+      end
+      puts "#{'+-----' * 11}+-----+"
+    end
   end
 end
