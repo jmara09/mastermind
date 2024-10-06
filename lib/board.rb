0950e57c3540498bd78f112aca2636e6ccde2afe
Jmara <jmara.0923@gmail.com>
2024-10-06 09:55:49 +0400

    Add space between colors

--- lib/board.rb
+++ lib/board.rb
@@ -2,13 +2,15 @@
 
 module Mastermind
   class Board
-    COLOR = %w[red blue white black green yellow magenta].freeze
-    TINY_PEG = %w[white black].freeze
+    COLORS = %w[red blue white black green yellow magenta].freeze
+    TINY_PEG = %w[white red].freeze
+
+    attr_reader :checker
 
     def initialize
-      @secret_code = []
-      @board_guess_accuracy = Array.new(12, Array.new(4, '0'))
-      @board_guess_pegs = Array.new(12, Array.new(4, '0'))
+      @board_tiny_pegs = Array.new(12, Array.new(4, ' '))
+      @board_guess_pegs = Array.new(12, Array.new(4, '   '))
+      @checker = []
       @guess = []
       @tries = 0
     end
@@ -18,21 +20,36 @@ module Mastermind
       @guess << code
     end
 
+    def accumulate_checker(code)
+      @checker << code
+    end
+
     def game_board
-      top_board(@board_guess_accuracy)
+      top_board(@board_tiny_pegs)
       bottom_board(@board_guess_pegs)
     end
 
-    def colorize_bottom
-      @board_guess_pegs = @board_guess_pegs.map.with_index do |column, i|
+    def colorize_top
+      @board_tiny_pegs = @board_tiny_pegs.map.with_index do |column, i|
+        next column if i > @tries - 1
+
         column.map.with_index do |pegs, j|
-          if i <= @tries - 1
-            pegs.colorize(@guess[i][j].to_sym)
+          if @checker[i][j] == 'red' || @checker[i][j] == 'white'
+            pegs.colorize(background: @checker[i][j].to_sym, mode: :bold)
           else
             pegs
           end
         end
-        # break if idx == @tries
+      end
+    end
+
+    def colorize_bottom
+      @board_guess_pegs = @board_guess_pegs.map.with_index do |column, i|
+        next column if i > @tries - 1
+
+        column.map.with_index do |pegs, j|
+          pegs.colorize(background: @guess[i][j].to_sym, mode: :bold)
+        end
       end
     end
 
@@ -40,7 +57,7 @@ module Mastermind
 
     def top_board(board)
       puts "#{'+-----' * 11}+-----+"
-      4.times do |i|
+      3.times do |i|
         12.times do |j|
           next unless (i + 1).odd?
 
@@ -49,8 +66,10 @@ module Mastermind
           else
             print "#{board[j][i]} #{board[j][i + 1]} | "
           end
+
           puts "\n" if j == 11
         end
+        puts "#{'|     ' * 12}|" if i == 1
       end
     end
 
@@ -59,14 +78,15 @@ module Mastermind
       4.times do |i|
         12.times do |j|
           if j == 11
-            print "|  #{board[j][i]}  |"
+            print "| #{board[j][i]} |"
           else
-            print "|  #{board[j][i]}  "
+            print "| #{board[j][i]} "
+
           end
         end
-        puts "\n"
+        puts "\n#{'|     ' * 12}|" if i != 3
       end
-      puts "#{'+-----' * 11}+-----+"
+      puts "\n#{'+-----' * 11}+-----+"
     end
   end
 end
